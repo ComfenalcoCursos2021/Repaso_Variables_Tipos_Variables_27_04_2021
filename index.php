036d25a5be9c15e0a7b544660106fba3cbc5dd26
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-04-27 16:43:23 -0500

    27 - 04 - 2021

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+    
+    
+    // Declarar 5 avariables que contengan sus informacion personal 
+    // y una constante, el dato al macenar no cambie
+
+    
+
+    // function saludar(string $p1 = "Miguel", int $p2 = 23){
+    //     return "Hola $p1 como estas tu edad es $p2 <br>";
+    // }
+    // echo saludar("Jaime", 25);
+
+
+    // echo saludar("Miguel");
+    // echo saludar("Diana");
+    // echo saludar("Jose");
+    
+
+    // $nombre = (string) "Miguel Angel Castro Escamilla";
+    // $respuesta = settype($nombre, 'int');
+    // var_dump($respuesta);
+    
+
+
+    // $respuesta = define("CEDULA", 123456789, true);
+    // echo CeDUlA;
+    // echo "<br>";
+    // echo $respuesta;
+
+    // const cedula = 123;
+    // cedula = 456;
+    // echo cedula;
+
+
+    // $vaso = (string) 'Agua';
+    // echo $vaso;
+    // echo '<br>';
+    // $vaso = (string) 'Cafe';
+    // echo $vaso;
+    // echo '<br>';
+    // $vaso = (int) 23;
+    // echo $vaso;
+
+    // $nombre = (string) 'Miguel Angel Castro Escamilla';
+    // echo $nombre;
+
+    
+
+
+
+?>
